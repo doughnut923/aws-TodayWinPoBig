54716d92beec66c3b61c8c0a5a720371ad6320ef
Trollinglolin <chanjeremyccy@gmail.com>
2025-09-21 02:23:01 +0800

    Call llm api

---
+++ backend/ai/llm.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+const OPENROUTER_API_KEY = process.env.OPENROUTER_API_KEY;
+
+console.log(OPENROUTER_API_KEY);
+
+async function callLLM(text) {
+  if (typeof text != 'string') {
+    throw new Error("Input must be a string")
+  }
+  try {
+    const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${OPENROUTER_API_KEY}`
+      },
+      body: JSON.stringify({
+        "model": "x-ai/grok-4-fast:free",
+        "messages": [
+          {
+            "role": "user",
+            "content": [
+              {
+                "type": "text",
+                "text": text
+              }
+            ]
+          }
+        ]
+      })
+    });
+
+    console.log(response);
+
+    const data = await response.json();
+    console.log(JSON.stringify(data, null, 2));
+    
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+module.exports = {
+  callLLM
+}
+
+callLLM("Give me a healthy meal plan for a day");
